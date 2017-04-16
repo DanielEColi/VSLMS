f74845b37f20a219c2cc1ef801169218c98b1790
Lin Meng Han <linmh0130@stu.xjtu.edu.cn>
2017-04-16 23:47:08 +0800

    Create simLMS.m

---
+++ simLMS.m
@@ -0,0 +1,40 @@
+% simLMS.m --- Designed by linmh0130@stu.xjtu.edu.cn
+%
+% Description
+%   function [y,e,W] = simLMS(x,d,mu,W0)
+%   function [y,e,W] = simLMS(x,d,mu,W0,v)
+%       A simple LMS Adaptive Filter
+% Update equation
+%       e(i) = d(i) - y(i);
+% or    e(i) = d(i) + v(i) - y(i);
+%
+%       W(next) = W(now) + (mu*e(i)).*u;
+% Parameters
+%   x:          Input signal
+%   d:          Desired output
+%   mu:         Stepsize
+%   W0:         Initial value of weights
+% Return
+%   y:          Output signal
+%   e:          Error
+%   W:          Weights
+
+function [y,e,W] = simLMS(x,d,mu,W0,varargin)
+error(nargchk(4,5,nargin));
+if (nargin == 5)
+    v = varargin{1};
+else
+    v = zeros(1,length(x));
+end
+W = W0;
+L = length(W);
+y = zeros(1,length(x));
+e = zeros(1,length(x));
+u = zeros(1,L); % input_sav vector
+for i = 1 : length(x)
+    u(2:L) = u(1:L-1);
+    u(1) = x(i);
+    y(i) = W * u';
+    e(i) = d(i) + v(i) - y(i);
+    W = W + (mu*e(i)).*u;
+end
